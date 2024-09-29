c20f1bc26215a763c65897b85e08a781716d08f9
Alexander Oh <oh.a@gmx.at>
2024-09-29 20:47:28 +0200

    move to meson

--- tests/test_simple.cc
+++ tests/test_simple.cc
@@ -3,6 +3,8 @@
 #include <string>
 using std::string;
 
+#include "sudoku.h"
+
 const char *actualValTrue  = "hello gtest";
 const char *actualValFalse = "hello world";
 const char *expectVal      = "hello gtest";
@@ -13,4 +15,18 @@ TEST(StrCompare, CStrEqual) {
 
 TEST(StrCompare, CStrNotEqual) {
     EXPECT_STRNE(expectVal, actualValFalse);
+}
+
+
+TEST(Bs, Simple) {
+    bs bs = ALL;
+
+    bs.set(5);
+    bs.set(6);
+    bs.print();
+    bs.clear(5);
+    bs.print();
+    
+    assert(bs.isSet(6));
+
 }
