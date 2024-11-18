e60b26a953f72565b1582ee05fbdca4e11aed9c8
Alexander Oh <oh.a@gmx.at>
2024-11-18 21:27:30 +0100

    add logger to main

--- main.cc
+++ main.cc
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
 #include "sudoku.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
+
+  auto console = spdlog::stdout_color_mt("console");
+  spdlog::set_default_logger(console);
+
   field_t input;
   from_file(std::cin, input);
 
